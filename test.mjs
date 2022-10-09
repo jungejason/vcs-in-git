e74abc4fa253c85e86d0aa585049ba0914fad99d
Jason Ge <gejun_1978@yahoo.com>
2022-10-09 09:57:35 -0700

    V3: add test for rand
    Summary: AT
    
    Test: `node test.mjs` passed

---
+++ test.mjs
@@ -0,0 +1,15 @@
+import { rand } from './utils.mjs'
+
+// Run the function 10 times. These are the best test cases ever!
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+console.assert(rand(1, 10) >= 1 && rand(1, 10) <= 10, 'rand(1, 10) is between 1 and 10');
+
