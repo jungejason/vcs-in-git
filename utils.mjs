f7c3a78720f8af728526012a762fab10d01882bc
Jason Ge <gejun_1978@yahoo.com>
2022-10-09 10:08:05 -0700

    V4: add getTime function
    Summary: AT
    
    Test: `node utils.mjs` passed

--- utils.mjs
+++ utils.mjs
@@ -1,6 +1,16 @@
 const rand = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 console.debug(rand(1, 10));
 
+const getTime = () => {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${hours}:${minutes}:${seconds}`;
+}
+console.log(getTime())
+
 export {
     rand,
+    getTime,
 }
